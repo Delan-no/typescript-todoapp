dd1edfd22a029dac8fd1a3523912fe79caaa0626
Délanno Kotcho <delanktc24@gmail.com>
2024-07-12 17:49:00 +0200

    todo main crée avec succès

--- src/components/TodoMain.vue
+++ src/components/TodoMain.vue
@@ -1,7 +1,28 @@
 <template>
-  <div>todo main</div>
+  <main class="main">
+    <ul class="todo-list">
+      <li :class="{ completed: todo.completed }" v-for="todo in taches" :key="todo.id">
+        <div class="view">
+          <input type="checkbox" class="toggle" />
+          <label for="">{{ todo.title }}</label>
+          <button class="destroy"></button>
+        </div>
+      </li>
+    </ul>
+  </main>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import 'todomvc-app-css/index.css'
+interface Todo {
+  id: number
+  title: string
+  completed: boolean
+}
+// const props = defineProps(['taches'])
+const props = defineProps<{
+  taches: Todo[]
+}>()
+</script>
 
 <style scoped></style>
