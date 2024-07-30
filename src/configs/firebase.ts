25c23ef765cef0134e23aa2e1e9500eb86403891
Délanno Kotcho <delanktc24@gmail.com>
2024-07-30 11:33:14 +0200

    des mofifications apportées

---
+++ src/configs/firebase.ts
@@ -0,0 +1,16 @@
+import { initializeApp, type FirebaseOptions } from 'firebase/app'
+
+const firebaseConfig: FirebaseOptions = {
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+// Initialiser le service d'authentification
+
+export { firebaseConfig }
